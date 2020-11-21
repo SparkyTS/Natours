9a9f7ca90d614bc21c8be3eaf98c4e5557ce3260
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-22 04:27:12 +0530

    created api to add tour

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -11,12 +11,30 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json(
-    { status: 'success',
+    {
+      status: 'success',
       results: tours.length,
       data: { tours }
     });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
