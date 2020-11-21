e2cf2b21bc8cc581d9f92dffd66de74cb446e4dc
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-22 04:01:36 +0530

    tested apis

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200)
+     .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
