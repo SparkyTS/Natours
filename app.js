4f09d4f9357814cabe7b56f0b625b0af2aa47002
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-22 05:00:03 +0530

    created api to update tour by id

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const tours = JSON.parse(
+let tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
@@ -42,6 +42,32 @@ app.post('/api/v1/tours', (req, res) => {
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+  const id = req.params.id * 1;
+  const tour = tours.find(t => t.id === id);
+  if (!tour) {
+    return res.status(400).json(
+      {
+        status: 'fail',
+        message: 'Invalid ID'
+      });
+  }
+
+  tours = tours.filter(t => t.id !== id)
+  const newTour = Object.assign(tour, req.body);
+  tours.push(newTour);
+  tours.sort((t1, t2) => t1.id - t2.id);
   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
     res.status(201).json({
       status: 'success',
