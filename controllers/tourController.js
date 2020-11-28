69f6a8538acfc47e08d733fe6468eafe8493b74f
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-29 00:40:02 +0530

    refactored method for updating tour from db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,26 +55,23 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // const id = req.params.id * 1;
-  // // tour filtered from the check id middleware
-  // const tour = req.tour;
-  // // removing the tour which needs to be updated
-  // tours = tours.filter(t => t.id !== id);
-  // // updating the tour and adding back to tours
-  // const newTour = Object.assign(tour, req.body);
-  // tours.push(newTour);
-  // // sorting the tours based on tour id before saving to the file ( not sorting the data would break the code for creating tour )
-  // tours.sort((t1, t2) => t1.id - t2.id);
-  // // finally storing updated tours in the file and returning the response
-  // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-  //   res.status(201).json({
-  //     status: 'success',
-  //     data: {
-  //       tour: newTour
-  //     }
-  //   });
-  // });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json(
+      {
+        status: 'success',
+        data: { tour }
+      });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
