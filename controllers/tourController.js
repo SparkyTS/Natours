c296d61c1c72b9c246e96707ebeb4c01e7c085de
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-29 03:02:43 +0530

    applied basic filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,22 @@ const tours = [];
 //writing actual handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(ef => delete queryObj[ef]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //                    .where('duration').equals(5)
+    //                    .where('difficulty').equals('easy')
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json(
       {
         status: 'success',
