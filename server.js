c7f30dc1fbad5dab2ed54f02abf5dcf7fdaf11e9
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-30 05:15:30 +0530

    completed error handing

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...!');
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -16,7 +24,23 @@ mongoose
   }).then(con => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
   console.log(`App is running in ${process.env.NODE_ENV} environment`);
+});
+
+process.on('unhandledRejection', err => {
+  console.error('UNHANDLED REJECTION! 💥 Shutting down...!');
+  console.error(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...!');
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
