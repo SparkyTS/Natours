87f862a6abcccb398babd26ff1eaf71898b7e057
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-12-01 03:03:07 +0530

    created endpoint for user sign up

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
