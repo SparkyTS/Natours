55a6c73a4a654da05672d6829af7dd0ba7174d00
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-12-02 01:48:05 +0530

    added code for generating reset password token

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -37,7 +38,9 @@ const userSchema = new mongoose.Schema({
       message: `Passwords doesn't match`
     }
   },
-  passwordChangedAt: Date
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -61,6 +64,15 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
   const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
   return JWTTimestamp < changedTimestamp;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
