d385287f822aa276d4fc413c84a3aa36f67c8e99
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-12-06 04:46:39 +0530

    embedding user data in tour Note: the code in this commit will be removed It's just for testing the embedding of data

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+const User = require('./userModel');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -100,7 +101,8 @@ const tourSchema = new mongoose.Schema({
       description: String,
       day: Number
     }
-  ]
+  ],
+  guides: Array
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
@@ -116,10 +118,11 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// tourSchema.pre('save', function(next) {
-//   console.log('Will save document...');
-//   next();
-// })
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 //
 // runs after .save() and .create()
 // tourSchema.post('save', function(doc, next) {
