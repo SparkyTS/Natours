0f6c18a3b8ab4c67822eb0597de8e8bd7ba1b1d7
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-12-09 04:01:39 +0530

    added endpoints to create and get reviews

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoute');
 
 const app = express();
 
@@ -64,6 +65,7 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
