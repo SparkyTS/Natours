01221dce39482649d79abe96b9f9835fc1d43abc
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-12-10 18:52:46 +0530

    added me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
+router.get('/me', authController.protect, userController.getMe, userController.getUserById);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
